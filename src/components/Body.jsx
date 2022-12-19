a0db293554860b093956466c8e8f462a627a1e53
darikmohammed <r.m.darik@gmail.com>
2022-12-19 13:31:06 +0300

    Add body component

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './Home';
+
+function Body() {
+  return (
+    <div className="px-40 h-full bg-body">
+      <Home />
+      <h1 className=" h-96">Body</h1>
+    </div>
+  );
+}
+
+export default Body;
