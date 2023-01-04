25f6e79522a9e2e014359e6c9655b0b219794108
darikmohammed <r.m.darik@gmail.com>
2023-01-04 23:47:46 +0300

    Finish up the blogs

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Card from './Blogs/Card';
+import data from '../data/data';
 
 function Blogs() {
   return (
     <section class=" bg-white flex flex-col items-center py-16 px-40 gap-8">
       <h1 class=" text-h4 font-bold">Blogs</h1>
       <div class="flex flex-nowrap px-2 overflow-x-auto no-scrollbar gap-4 snap-x snap-mandatory">
-        <Card />
+        {data.blogs.map((blog) => (
+          <Card
+            Image={blog.Image}
+            Name={blog.Name}
+            Food={blog.Food}
+            Birr={blog.Birr}
+            Location={blog.Location}
+          />
+        ))}
       </div>
     </section>
   );
--- src/components/Blogs/Card.jsx
+++ src/components/Blogs/Card.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import data from '../../data/data';
-console.log(data.blogs);
-function Card() {
-  return data.blogs.map((blog) => (
-    <div class=" flex-none snap-always snap-center bg-black_light p-4 rounded-lg flex flex-col gap-4">
-      <img src="./img/blogs/Majet.png" alt="Majet" />
+
+function Card({ Image, Name, Food, Birr, Location }) {
+  return (
+    <div class=" flex-none snap-always snap-center bg-black_light p-4 rounded-lg flex flex-col justify-between gap-4">
+      <img src={`./img/blogs/${Image}.png`} alt={Name} />
       <div>
-        <h1 class=" font-bold text-xl">Majet Restaurant</h1>
+        <h1 class=" font-bold text-xl">{Name}</h1>
         <ul class="list-disc px-6">
-          <li>Majest Special Combo</li>
-          <li>300ETB (including Vat)</li>
-          <li>On the way from Atote to Welde Amanuel </li>
+          <li>{Food}</li>
+          <li>{Birr}</li>
+          <li>{Location}</li>
         </ul>
       </div>
       <button class="rounded-lg border-solid border-2 border-primary_light py-3 px-5 font-semibold">
         Get Direction
       </button>
     </div>
-  ));
+  );
 }
 
 export default Card;
