0a33c03c11b1cd08a59641c50ac8610ec8d76375
darikmohammed <r.m.darik@gmail.com>
2023-01-05 23:55:30 +0300

    Update APP

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Nav from './components/Nav';
 function App() {
   return (
     <div className="font-poppin">
-      <header className="sticky top-0 z-10">
+      <header className="sticky top-0 z-10 w-full">
         <Nav />
       </header>
       <main>
