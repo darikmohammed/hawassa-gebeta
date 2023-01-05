eafdb5e29ee724a21b947259132dcc69010534b3
darikmohammed <r.m.darik@gmail.com>
2023-01-05 23:56:02 +0300

    Update Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 function Nav() {
   return (
-    <nav className="px-40 py-8 flex justify-between items-center shadow-lg shadow-slate-400/10 bg-white">
+    <nav className="md:px-40 py-8 flex justify-between items-center shadow-lg shadow-slate-400/10 bg-white">
       <div className="logo flex items-center gap-3">
         <img src="./img/logo.png" alt="Logo" />
         <h1 className=" text-h6 font-bold">Hawassa Gebeta</h1>
       </div>
-      <ul className="flex gap-10">
+      <ul className="hidden md:flex gap-10">
         <li>Home</li>
         <li>About us</li>
         <li>Blogs</li>
         <li>Contact</li>
       </ul>
-      <button className="border-solid border-2 border-black rounded-lg py-3 px-5">
+      <button className="hidden md:block border-solid border-2 border-black rounded-lg py-3 px-5">
         Contact us
       </button>
     </nav>
