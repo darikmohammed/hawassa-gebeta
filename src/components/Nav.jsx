f739943794a61e01fc3e0292de0fb20e5293c049
darikmohammed <r.m.darik@gmail.com>
2023-01-06 23:57:11 +0300

    Update nav menu for mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 function Nav() {
   return (
-    <nav className="md:px-40 py-8 flex justify-between items-center shadow-lg shadow-slate-400/10 bg-white">
+    <nav className=" px-4 py-8 flex justify-between items-center shadow-lg shadow-slate-400/10 bg-white md:px-40">
       <div className="logo flex items-center gap-3">
-        <img src="./img/logo.png" alt="Logo" />
-        <h1 className=" text-h6 font-bold">Hawassa Gebeta</h1>
+        <img
+          src="./img/logo.png"
+          alt="Logo"
+          className=" w-[26.68px] h-[26px]"
+        />
+        <h1 className=" text-base md:text-h6 font-bold">Hawassa Gebeta</h1>
+      </div>
+      <div className=" block md:hidden">
+        <img src="./img/Menu.png" alt="hamburger" />
       </div>
       <ul className="hidden md:flex gap-10">
         <li>Home</li>
