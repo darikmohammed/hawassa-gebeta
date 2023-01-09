bf47514cc41f78ccc1218ee9d6b10f0763856255
darikmohammed <r.m.darik@gmail.com>
2023-01-09 21:43:26 +0300

    Update works section for mobile

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Work() {
   return (
-    <section class="bg-white flex flex-col px-40 items-center pt-16 pb-16 gap-8">
-      <h1 class=" text-h6 font-bold">Working with</h1>
+    <section class="bg-white px-8 flex flex-col md:px-40 items-center pt-16 pb-16 gap-8">
+      <h1 class=" text-center text-h6 font-bold">Working with</h1>
       <div class="flex items-center">
-        <ul class="flex gap-8 relative grayscale opacity-30 items-center">
+        <ul class="flex flex-col md:flex-row gap-8 relative grayscale opacity-30 items-center">
           <img src="./img/partnership/Ker-awud.png" alt="KER-AWUD Hotel" />
           <img src="./img/partnership/Negus.png" alt="Negus" />
           <img src="./img/partnership/Rori-Hotel.png" alt="Rori Hotel" />
