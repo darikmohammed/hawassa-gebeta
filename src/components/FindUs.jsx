1f788c1a05e0b64c976af46753c902eb4e2a8d27
darikmohammed <r.m.darik@gmail.com>
2023-01-10 23:42:03 +0300

    Add Findus component

---
+++ src/components/FindUs.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Map from './Map';
+
+function FindUs() {
+  return (
+    <section className="bg-white flex flex-col md:px-40 items-center py-16 gap-8">
+      <h1 className=" text-center text-[16px] font-bold md:text-[32px] ">
+        Find restaurant near you in Hawassa
+      </h1>
+      <div className=" bg-black_light flex flex-col self-stretch items-center rounded-xl py-[32px] md:py-[64px]">
+        <Map />
+        <div></div>
+      </div>
+    </section>
+  );
+}
+
+export default FindUs;
