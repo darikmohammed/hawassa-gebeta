c5814a1532dcffa9757f3f7e5f362ec57276da4a
darikmohammed <r.m.darik@gmail.com>
2023-01-10 23:19:28 +0300

    Update blogs

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -6,16 +6,18 @@ function Blogs() {
   return (
     <section class=" bg-white flex flex-col items-center py-16 px-40 gap-8">
       <h1 class=" text-h4 font-bold">Blogs</h1>
-      <div class="flex flex-nowrap px-2 overflow-x-auto no-scrollbar gap-4 snap-x snap-mandatory">
-        {data.blogs.map((blog) => (
-          <Card
-            Image={blog.Image}
-            Name={blog.Name}
-            Food={blog.Food}
-            Birr={blog.Birr}
-            Location={blog.Location}
-          />
-        ))}
+      <div class="flex overflow-x-scroll pb-10 hide-scroll-bar">
+        <div className="flex flex-nowrap mx-8 md:mx-40">
+          {data.blogs.map((blog) => (
+            <Card
+              Image={blog.Image}
+              Name={blog.Name}
+              Food={blog.Food}
+              Birr={blog.Birr}
+              Location={blog.Location}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
