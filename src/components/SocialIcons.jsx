bee3144d2c18327886dcf601f0ae211d115b5a73
darikmohammed <r.m.darik@gmail.com>
2023-01-13 22:06:26 +0300

    Add footer social icons

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function SocialIcons() {
-  return <div>SocialIcons</div>;
+  return (
+    <div>
+      <ul className="flex gap-2">
+        <li>
+          <img src="./img/footer/instagram.png" alt="Instagram" />
+        </li>
+        <li>
+          <img src="./img/footer/facebook.png" alt="Facebook" />
+        </li>
+        <li>
+          <img src="./img/footer/telegram.png" alt="Telegram" />
+        </li>
+        <li>
+          <img src="./img/footer/tiktok.png" alt="TikTok" />
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default SocialIcons;
