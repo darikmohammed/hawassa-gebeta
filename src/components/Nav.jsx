eb2a90eda9f10c38fdd8a70c8270eea57ffab6bb
darikmohammed <r.m.darik@gmail.com>
2023-01-23 23:37:55 +0300

    Add open modifier for the nav hamburger

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Nav() {
+  const [isOpen, setisOpen] = useState(false);
   return (
     <nav className=" px-8 py-8 flex justify-between items-center shadow-lg shadow-slate-400/10 bg-white md:px-40">
       <div className="logo flex items-center gap-3">
@@ -11,14 +12,14 @@ function Nav() {
         />
         <h1 className=" text-base md:text-h6 font-bold">Hawassa Gebeta</h1>
       </div>
-      <div className=" block md:hidden">
+      <div className=" block md:hidden" onClick={() => setisOpen(!isOpen)}>
         <img src="./img/Menu.png" alt="hamburger" />
       </div>
-      <ul className="hidden md:flex gap-10">
-        <li>Home</li>
-        <li>About us</li>
-        <li>Blogs</li>
-        <li>Contact</li>
+      <ul className="hidden md:flex gap-10" isOpen={isOpen}>
+        <li onClick={() => setisOpen(false)}>Home</li>
+        <li onClick={() => setisOpen(false)}>About us</li>
+        <li onClick={() => setisOpen(false)}>Blogs</li>
+        <li onClick={() => setisOpen(false)}>Contact</li>
       </ul>
       <button className="hidden md:block border-solid border-2 border-black rounded-lg py-3 px-5">
         Contact us
